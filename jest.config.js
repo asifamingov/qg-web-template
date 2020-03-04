ade79d0daf85cd638f55ff5d8b9a2f66d471a73a
AsifAmin <asif_udr@yahoo.com>
2020-03-04 10:05:19 +1000

    webpack updates

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  setupFilesAfterEnv: ['./jest.setup.js'],
   globalSetup: './e2e/config/setup.js',
   globalTeardown: './e2e/config/teardown.js',
   testEnvironment: './e2e/config/puppeteer_environment.js',
