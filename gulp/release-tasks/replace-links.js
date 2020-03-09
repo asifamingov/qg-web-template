80cf9879a95f79cfcbc0863b069a9f7aca9cef49
AsifAmin <asif_udr@yahoo.com>
2020-03-09 17:32:05 +1000

    swe4 setup

--- gulp/release-tasks/replace-links.js
+++ gulp/release-tasks/replace-links.js
@@ -1,14 +1,18 @@
 'use strict';
-
 module.exports = function (gulp, plugins, es, config) {
+  var replace = '\\/assets\\/versionName\\/';
+  var re = new RegExp(replace, 'g');
+
+  var replace2 = '\\/assets\\/v4\\/';
+  var re2 = new RegExp(replace2, 'g');
   return function (cb) {
     return es.merge([
       gulp.src(`${config.basepath.release}/docs/*.html`)
-        .pipe(plugins.replace(/\/assets\/v3.1\//g, 'https://static.qgov.net.au/assets/v3.1/'))
+        .pipe(plugins.replace(re, `${config.webpath}${config.versionName}/`))
         .pipe(gulp.dest(`${config.basepath.release}/docs/`)),
 
       gulp.src(`${config.basepath.release}/docs/pagemodels/*.html`)
-        .pipe(plugins.replace(/\/assets\/v3.1\//g, 'https://static.qgov.net.au/assets/v3.1/'))
+        .pipe(plugins.replace(re2, `${config.webpath}${config.versionName}/`))
         .pipe(gulp.dest(`${config.basepath.release}/docs/pagemodels/`)),
     ]);
   };
